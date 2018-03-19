7527b281454c359dc1f4ca556e223ce7a158b847
fadfda <fadfda>
2018-03-19 21:03:45 +0530

    dfsda

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+<body bgcolor="#FFFF00">
+<center><h1>DEV</h1></center>
+<br><br>
+<center><h2>APP NAME IS --> <?php echo getenv("appname"); ?></h2></center>
+<center><h2>ENV NAME IS --> <?php echo getenv("environment"); ?></h2></center>
+</body>
+</html>
